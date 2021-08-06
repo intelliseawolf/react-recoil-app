e2b684c2f6684398ebde2f5670da1828946912ed
intellieseawolf <intelliseawolf103@gmail.com>
2021-08-06 10:21:32 -0400

    added: todo list

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,70 @@
+import { atom, useRecoilValue, selector } from "recoil";
+import TodoItemCreator from "./TodoItemCreator";
+import TodoItem from "./TodoItem";
+import TodoListFilters from "./TodoListFilters";
+import TodoListStats from "./TodoListStats";
+
+export const todoListState = atom({
+  key: "todoListState",
+  default: [],
+});
+
+export const todoListFilterState = atom({
+  key: "todoListFilterState",
+  default: "Show All",
+});
+
+const filteredTodoListState = selector({
+  key: "filteredTodoListState",
+  get: ({ get }) => {
+    const todoList = get(todoListState);
+    const filter = get(todoListFilterState);
+
+    switch (filter) {
+      case "Show Completed":
+        return todoList.filter((todo) => todo.isComplte);
+
+      case "Show Uncompleted":
+        return todoList.filter((todo) => !todo.isComplte);
+
+      default:
+        return todoList;
+    }
+  },
+});
+
+export const todoListStatsState = selector({
+  key: "TodoListStats",
+  get: ({ get }) => {
+    const todoList = get(todoListState);
+    const totalNum = todoList.length;
+    const totalCompletedNum = todoList.filter((item) => item.isComplete).length;
+    const totalUncompletedNum = totalNum - totalCompletedNum;
+    const percentCompleted =
+      totalNum === 0 ? 0 : (totalCompletedNum / totalNum) * 100;
+
+    return {
+      totalNum,
+      totalCompletedNum,
+      totalUncompletedNum,
+      percentCompleted,
+    };
+  },
+});
+
+const TodoList = () => {
+  const todoList = useRecoilValue(filteredTodoListState);
+
+  return (
+    <>
+      <TodoListStats />
+      <TodoListFilters />
+      <TodoItemCreator />
+      {todoList.map((todoItem) => (
+        <TodoItem key={todoItem.id} item={todoItem} />
+      ))}
+    </>
+  );
+};
+
+export default TodoList;
